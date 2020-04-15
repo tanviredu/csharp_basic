4579153f6c77680c81a3e5f027f0359e0490b4fc
Tanvir Rahman <ornobtanvir.git@gmail.com>
2020-04-15 20:53:55 +0600

    added

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -1,10 +1,112 @@
+using System;
+using System.Collections.Generic;
+
 namespace GradeBook
 {
     public class Book
     {
+        
+        // change the constructor
+        // and add a parameter
+        public Book(string name){
+            // this is the constructor
+            // function
+            // now i create a List in thee
+            // this object is autometically generated here
+            // now in this valriable
+            // all the grades will be added
+            // now can we directly access this 
+            // or we need a method to access this
+            // to do this when you declare a list
+            // you need to add the public access modifier 
+            // this class the generic list class
+            // but this is not a good idea
+            // do not let people access the data structure
+            // that you are using from different class
+            //use public method
+            // and only let user remove or add data with this method
+
+            grades = new List<double>();
+            this.name = name;
+        }
+        
+
+        // here you define the list
+        // and inside the constructor
+        // unlike other scripting
+        //language you can write things 
+        //later because this is a compiled language 
+
+        private  List<double>grades;
+        private string name; // define a variable for the name of the book
+
+        // when you use the private
+        // other class cant use it
+        // only this class can use this
+        // add CTRL+. to add the namespace            
+    
+        // now any method you add can access the 
+        // grades
+
+        
+        
         // this class is under the 
         // Gradebook
         // thats why we need the name 
         // namespace gradebook
+        // we can put different class
+        // in the same file
+        // but in csharp we dont do that
+        //we create separate class
+        // in separate file
+        //class has properties and method
+        // the propertise are also called state
+        // and the method is also called the behaviour
+        public void AddGrade(double grade){
+
+            // in ghere you need to store the grade
+            // add the state/propertise/field
+            // if you want that the properties 
+            // will be used througout the program
+            // you can not use it under a method
+            // and you cant use implecit typing
+            // now add the graders in the list
+
+            grades.Add(grade);
+
+        }
+
+        public void showstat(){
+            // all the minimum value 
+            // maximum value
+            // and the avg value will be here
+            var result = 0.0;
+            var highgrade = double.MinValue; // a very low value
+            var lowgrade = double.MaxValue;  // a very high value
+
+            // calculayte this with a loop
+
+            foreach(var number in grades){
+                // you can use the grades because
+                // its in the class
+                lowgrade = Math.Min(number,lowgrade);
+                highgrade = Math.Max(number,highgrade);
+                result+=number;
+
+
+            }
+
+            // now find the avg
+            result = result/grades.Count;
+
+            // now console log this
+
+            Console.WriteLine($"The lowest Grade is {lowgrade}");
+            Console.WriteLine($"The highest Grade is {highgrade}");
+            Console.WriteLine($"The avg Grade is {result:N2}");
+
+        }
+
+        
     }
 }
--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -9,174 +9,20 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            // create a object 
+            // in the book
+            var book = new Book("my fav book");
+            book.AddGrade(82.4);
+            book.AddGrade(32.4);
+            book.AddGrade(32.4);
+            book.AddGrade(52.4);
+            book.AddGrade(64.4);
+            book.AddGrade(62.4);
+            book.showstat();
 
+        
 
-
-            // create the object of the class Book
-
-
-            // main is the entry point of the code
-            //Console.WriteLine("Hello World!");
-            //Console.WriteLine("Hello tanvir Rahman");
-            // adding the argument with the program
-            // adding  a if statement
-
-            if(args.Length >0){
-                
-                // adding  a variable
-                // now anything you do with a with a floating 
-                // point is actually considered as double
-                // and you cant use it as a float type
-                // until you put a F at the end of it
-                // if you dont use the F 
-                // you may have to use the 
-                // double type 
-                // insted of the float
-                float x;
-                x = 34.1F;
-                Console.WriteLine("th value of x is :");
-                Console.WriteLine(x);
-            
-                Console.WriteLine("Hello "+args[0]+ "!!!!");
-                // adding the string interpolation
-                // try to use the dotnet interpolation
-                // it is very easy
-                // like format in python
-                Console.WriteLine($"Hello , {args[0]} !!!!" );
-            }else{
-                Console.WriteLine("hello unknown person");
-            }
-
-
-            // adding two number
-            // now you can use the iimplicit typing
-            var number1  = 100.00;
-            var number2 = 2.23;
-            var ans = number1+number2;
-
-            Console.WriteLine($"The addtion is {ans}");
-            
-
-            // now we can declare an array
-            // with ths command
-            // you must enter the array size in this number
-            var numbers = new double[3];
-            // populating the array
-            // with the number
-            numbers[0] = 12.7;
-            numbers[1] = 100.0;
-            numbers[2] = 500.3;
-
-
-            // instedof adding number like this 
-            // you can directly add the array
-            // with this method
-
-
-            // and the array command is the 
-            // var name = new [];
-            // thats it
-            var myarray = new double[3] {100.0,22.0,2.0};
-
-            // now if you dont want to provide the explicit number
-            // of length you can leave it blank
-
-            var myarray2 = new double[] {11,223.33,44,2.223,23.23,2323.23};
-            Console.WriteLine(myarray2.Length);
-
-            // now adding the number
-            var result = numbers[0];
-            result = result+numbers[1];
-            result = result+numbers[2];
-            Console.WriteLine(result);
-
-
-            Console.WriteLine(myarray.Length);
-
-            // you dont even need to use the 
-            // double or anything 
-            var another_number = new [] {100,200,300,400};
-            Console.WriteLine(another_number.Length);
-            Console.WriteLine(another_number.GetType());
-
-            var num = new[] {133,11.4,334.343,23.2323};
-            Console.WriteLine(num.Length);
-            Console.WriteLine(num.GetType());
-
-            // insted of manually add the number 
-            // we can use  aforeach loop 
-            // to do  that work
-            result = 0.0;
-            foreach(var number in numbers ){
-                // this you can add it
-                result+=number;
-
-            }            
-
-            Console.WriteLine(result);
-
-            // this array is hard to
-            // for the calculation
-            // you may add list
-            // like data structure
-            // for that
-
-
-            // this is how you use the generic data structure
-            // this is how you use most of the type
-            // the syntax is
-            // var name = new List<type>;
-            // thats it
-            var grades = new List<double>() {122,33.3434,3434.34,3434,343,6563.34};
-            // you can initializa this array easily
-            /// with this help
-            // and you can manually add this
-            // to  the array
-
-            // and you can add number with the loop too
-            var result2 = 0.0;
-            
-            // then print the number
-            // print the number
-
-            foreach(var grade in grades){
-                // now we are going to print 
-                // this from the list
-                //Console.WriteLine(grade);
-                //add the whole value
-                // with the array
-                result2+=grade;
-
-            }
-
-            Console.WriteLine(result2);
-            
-            // now we will find the 
-            // the average 
-            // of the result
-            // the list object 
-            //has a property name count
-
-            var result3 = result2/grades.Count;
-
-            Console.WriteLine($"the avg is {result3:N2}");
-
-            // you nedd to formatting
-            // this value
-            // the N3 is the formatting 
-            // the value
-
-
-
-
-
-
-            //grades.Add(56.1);
-            // you can index the list just like the array
-            // array is dynamic
-            // you can even apply yhe binary search 
-            // in the List if the list is sorted
-
+           
 
 
 
