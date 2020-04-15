a33077f865228948fe2bccdd60e414372d90ac0f
Tanvir Rahman <ornobtanvir.git@gmail.com>
2020-04-15 22:04:54 +0600

    added

---
+++ gradebook/test/gradebook.test/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace gradebook.test
+{
+    public class UnitTest1
+    {
+
+        // test method should have the attribute
+        // [Fact] the xunit will only invoke the 
+        // [Fact] method for testing
+        // other function will be considered as 
+        // a helper function
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
