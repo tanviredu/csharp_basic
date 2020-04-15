8c43c02bd3e92e32e9b3ac624871eb4d2106e5ce
Tanvir Rahman <ornobtanvir.git@gmail.com>
2020-04-15 16:31:43 +0600

    added

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -6,16 +6,50 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+
+            // main is the entry point of the code
             //Console.WriteLine("Hello World!");
             //Console.WriteLine("Hello tanvir Rahman");
             // adding the argument with the program
-            Console.WriteLine("Hello "+args[0]+ "!!!!");
+            // adding  a if statement
+
+            if(args.Length >0){
+                
+                // adding  a variable
+                // now anything you do with a with a floating 
+                // point is actually considered as double
+                // and you cant use it as a float type
+                // until you put a F at the end of it
+                // if you dont use the F 
+                // you may have to use the 
+                // double type 
+                // insted of the float
+                float x;
+                x = 34.1F;
+                Console.WriteLine("th value of x is :");
+                Console.WriteLine(x);
             
-            // adding the string interpolation
-            // try to use the dotnet interpolation
-            // it is very easy
-            // like format in python
-            Console.WriteLine($"Hello , {args[0]} !!!!" );
+                Console.WriteLine("Hello "+args[0]+ "!!!!");
+                // adding the string interpolation
+                // try to use the dotnet interpolation
+                // it is very easy
+                // like format in python
+                Console.WriteLine($"Hello , {args[0]} !!!!" );
+            }else{
+                Console.WriteLine("hello unknown person");
+            }
+
+
+            // adding two number
+            // now you can use the iimplicit typing
+            var number1  = 100.00;
+            var number2 = 2.23;
+            var ans = number1+number2;
+
+            Console.WriteLine($"The addtion is {ans}");
+            
+
+
 
         }
     }
