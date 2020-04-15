90c7a063e08c0a43868163027449cd96fb54a024
Tanvir Rahman <ornobtanvir.git@gmail.com>
2020-04-15 16:04:15 +0600

    first

---
+++ gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello tanvir Rahman");
+            // adding the argument with the program
+            Console.WriteLine("Hello "+args[0]+ "!!!!");
+            
+            // adding the string interpolation
+            // try to use the dotnet interpolation
+            // it is very easy
+            // like format in python
+            Console.WriteLine($"Hello , {args[0]} !!!!" );
+
+        }
+    }
+}
