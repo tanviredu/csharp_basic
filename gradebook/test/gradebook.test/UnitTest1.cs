2af896c813af672adb8cf1f753282db0d6111a71
Tanvir Rahman <ornobtanvir.git@gmail.com>
2020-04-16 16:47:40 +0600

    added

--- gradebook/test/gradebook.test/UnitTest1.cs
+++ gradebook/test/gradebook.test/UnitTest1.cs
@@ -14,6 +14,30 @@ namespace gradebook.test
         [Fact]
         public void Test1()
         {
+            // there are a number of assert static
+            // method in unit test assret class
+            // lets test with a  2 number
+            // with expected value and predicted value
+
+            var x = 5;
+            var y = 2;
+            var actual = x+y;
+            var expected = 7;
+            Assert.Equal(expected,actual);
+
+
+
+            // now basically unit test function divided into three
+            //section
+
+            // arrange [all the data arrange here]
+
+
+            // act [all the function will be there]
+
+
+            // assert [the main test is doing here] 
+
 
         }
     }
