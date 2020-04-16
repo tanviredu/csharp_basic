47ee7b0800816c6bc5ad6ce5d9d323ae1654afc1
Tanvir Rahman <ornobtanvir.git@gmail.com>
2020-04-16 18:43:56 +0600

    added

--- gradebook/test/GradeBook.Tests/TypeTests.cs
+++ gradebook/test/GradeBook.Tests/TypeTests.cs
@@ -48,6 +48,60 @@ namespace GradeBook.Tests
     // the first parameter is the Book
     // is the return type 
     // so we return a book object
+
+
+
+
+
+
+
+    // now make a test to change the propertise
+    // of the book
+
+    [Fact]
+    public void CanSetNameFromReference(){
+            // get the book first
+            var book1 = GetBook("Book 1");
+            // make a method to set the same
+            // dont write the direct code
+            // always make a method for it
+            SetName(book1,"New Book Name");
+    }
+
+        private void SetName(Book book, string name)
+        {
+            book.Name = name;
+            // yes you can change the 
+            // the propertise with refrence
+        }
+
+
+
+        // there is another way to do it
+
+        [Fact]
+        public void CsharpCanPassByRef(){
+            var book1 = GetBook("Book1");
+            // we are going to send the reference
+            // of the book
+            GetBookSetName(ref book1,"this is the new name");
+
+            Assert.Equal("this is the new name",book1.Name);
+        }
+
+        // this book will catch 
+        // the reference 
+        // and then set the name
+        // this ref Book book will tell 
+        // that catch a reference have to catch
+        private void GetBookSetName(ref Book book,string name)
+        {
+            // now we can set the name
+            // directly
+
+            book = new Book(name);
+        }
+
         Book GetBook(string name)
         {
             return new Book(name);
